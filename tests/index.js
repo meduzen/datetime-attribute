a69d5bad986ce572ce03789d7ee98e8e5e7e0241
Mehdi M <hi@mehdi.cc>
2021-11-01 00:37:18 +0100

    Fix test only passing on Brussels timezone, une fois

--- tests/index.js
+++ tests/index.js
@@ -162,10 +162,11 @@ describe('daysBetween', () => {
 })
 
 const summer = new DateTime(2021, 5, 21)
+const oneWeekAfterSummer = new DateTime(2021, 5, 28)
 
 describe('DateTime class', () => {
   test('extends Date', () => expect(summer).toBeInstanceOf(Date))
   test('.getWeekNumber', () => expect(summer.getWeekNumber()).toBe(25))
-  test('.setWeekNumber', () => expect(summer.setWeekNumber(26)).toBe(1624831200000))
+  test('.setWeekNumber', () => expect(summer.setWeekNumber(26)).toBe(oneWeekAfterSummer.getTime()))
   test(".to('day')", () => expect(summer.to('day')).toBe('2021-06-28'))
 })
