7b80e254c57e0f05dcdf9a452535cb66f70a1c90
Saber Bjeoui <bjeoui.saber@gmail.com>
2022-03-09 00:27:45 +0100

    generate and update type definition files

--- types/datetime.d.ts
+++ types/datetime.d.ts
@@ -8,23 +8,23 @@
  * See also: https://www.brucelawson.co.uk/2012/best-of-time/
  *
  * @param {Date=} date
- * @param {precisionKeyword=} precision
+ * @param {Precision=} precision
  * @returns {string}
  */
-export function datetime(date?: Date | undefined, precision?: precisionKeyword | undefined): string;
+export function datetime(date?: Date | undefined, precision?: Precision | undefined): string;
 /**
  * Create `datetime="2021-12-02T17:34-06:00"` attribute for `<time>`.
  *
  * @param {Date=} date
- * @param {precisionKeyword=} precision
+ * @param {Precision=} precision
  * @param {number=} offsetHours
  * @param {number=} offsetMinutes
  * @param {boolean=} inRealLifeBoundaries
  * @returns {string}
  */
-export function datetimeTz(date?: Date | undefined, precision?: precisionKeyword | undefined, offsetHours?: number | undefined, offsetMinutes?: number | undefined, inRealLifeBoundaries?: boolean | undefined, ...args: any[]): string;
-export function utc(date?: Date | undefined, precision?: precisionKeyword | undefined): string;
+export function datetimeTz(date?: Date | undefined, precision?: Precision | undefined, offsetHours?: number | undefined, offsetMinutes?: number | undefined, inRealLifeBoundaries?: boolean | undefined, ...args: any[]): string;
+export function utc(date?: Date | undefined, precision?: 'datetime' | 'datetime second' | 'datetime ms' | 'time' | 'second' | 'ms'): string;
 /**
  * Supported precision keywords.
  */
-export type precisionKeyword = 'day' | 'year' | 'yearless' | 'month' | 'week' | 'time' | 'second' | 'ms' | 'time utc' | 'second utc' | 'ms utc' | 'datetime' | 'datetime second' | 'datetime ms' | 'datetime utc' | 'datetime second utc' | 'datetime ms utc';
+export type Precision = 'day' | 'year' | 'yearless' | 'month' | 'week' | 'time' | 'second' | 'ms' | 'time utc' | 'second utc' | 'ms utc' | 'datetime' | 'datetime second' | 'datetime ms' | 'datetime utc' | 'datetime second utc' | 'datetime ms utc';
