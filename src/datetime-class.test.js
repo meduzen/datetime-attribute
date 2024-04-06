2a4d3d674d949a2a3191ddcaf526b74cd34d8c1d
Mehdi M <hi@mehdi.cc>
2024-04-06 12:50:07 +0200

    Try to trigger the Vitest GitHub action reporter with a failing test

--- src/datetime-class.test.js
+++ src/datetime-class.test.js
@@ -7,7 +7,7 @@ const twoWeeksAfterSummer = new DateTime(2021, 6, 5)
 
 describe('DateTime class', () => {
   test('extends Date', () => expect(summer).toBeInstanceOf(Date))
-  test('DateTime.getWeek', () => expect(summer.getWeek()).toBe(25))
+  test('DateTime.getWeek', () => expect(summer.getWeek()).toBe(215)) /** @todo: change me back from `215` (fail) to `25` (pass): this is for CI test purpose only */
   test("DateTime.to('day')", () => expect(summer.to('day')).toBe('2021-06-21'))
   test("DateTime.to('month')", () => expect(twoWeeksAfterSummer.to('month')).toBe('2021-07'))
   test('DateTime.setWeek()', () => expect(summer.setWeek(27)).toBe(twoWeeksAfterSummer.getTime()))
