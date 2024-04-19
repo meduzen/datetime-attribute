f396e6609b168905ea1f400f038e7d2f77878eec
Mehdi M <hi@mehdi.cc>
2024-04-19 21:12:17 +0200

    Add some notes in Temporal util

--- src/temporal/utils/temporal.js
+++ src/temporal/utils/temporal.js
@@ -1,4 +1,9 @@
 import { Temporal } from '@js-temporal/polyfill'
 // import { Temporal } from 'temporal-polyfill'
 
+// Date.prototype.toTemporalInstant = toTemporalInstant
+
 export const dateToPlainDate = d => new Temporal.PlainDate(d.getFullYear(), d.getMonth() + 1, d.getDate())
+
+/** @todo check https://tc39.es/proposal-temporal/docs/cookbook.html#date-only-values-legacy-date--temporalplaindate */
+// export const dateToPlainDate = d => d.toTemporalInstant().toZonedDateTimeISO(Temporal.Now.timeZoneId()).toPlainDate()
