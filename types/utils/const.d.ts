8c9fa80b54295f37b316773e5e5edf2a8c0eb7a5
Mehdi M <hi@mehdi.cc>
2024-04-20 14:49:53 +0200

    Document timezone offsets in code

--- types/utils/const.d.ts
+++ types/utils/const.d.ts
@@ -1,6 +1,15 @@
 export const MINUTES_PER_DAY: number;
 export const MILLISECONDS_PER_DAY: number;
 export const MILLISECONDS_PER_WEEK: number;
+/**
+ * The farthest timezone offset **behind** UTC time.
+ */
 export const REAL_LIFE_LOWER_TIMEZONE: number;
+/**
+ * The farthest timezone offset **ahead of** UTC time.
+ */
 export const REAL_LIFE_UPPER_TIMEZONE: number;
+/**
+ * Local timezone offset from UTC, in minutes.
+ */
 export const LOCAL_TZ_OFFSET: number;
